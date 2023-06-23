de3f9326829484757a9a82f86c9068f0a76db784
SaberFDev <nehir.efe.0508@gmail.com>
2023-06-23 13:26:09 +0300

    Final Version

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ const ytdl = require("ytdl-core");
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/info", async (req, res) => {
+  const { id } = req.query;
+  const videoInfo = await ytdl.getInfo("https://www.youtube.com/watch?v=" + id);
+  res.send(200, videoInfo.videoDetails);
+});
+
 app.get("/api/video", (req, res) => {
   const { id } = req.query;
   const videoReadableStream = ytdl("https://www.youtube.com/watch?v=" + id, {
@@ -13,7 +20,9 @@ app.get("/api/video", (req, res) => {
   });
   res.set(
     "content-disposition",
-    `attachment; filename="${"videoReadableStream" + ".mp4"}"`
+    `attachment; filename="${
+      Math.floor(Math.random() * 999).toString() + ".mp4"
+    }"`
   );
   videoReadableStream.pipe(res);
 });
@@ -21,14 +30,16 @@ app.get("/api/video", (req, res) => {
 app.get("/api/audio", async (req, res) => {
   const { id } = req.query;
   const url = "https://www.youtube.com/watch?v=" + id;
-  const info = await ytdl.getInfo(url);
-
   const videoReadableStream = await ytdl(url, {
     filter: "audioonly",
   });
-  res.header("Content-Disposition", 'attachment; filename="Audio.mp3');
-  res.send("Downloading");
-  const downloadFile = videoReadableStream.pipe(res);
+  res.set(
+    "content-disposition",
+    `attachment; filename="${
+      Math.floor(Math.random() * 999).toString() + ".mp4"
+    }"`
+  );
+  videoReadableStream.pipe(res);
 });
 
 app.listen(3000, () => {
